5f433b8a9f3521cbfac9028bce60367e9d797c9a
pc-b114-15\Eleve <antoine.depercin@student-cs.fr>
2023-01-09 15:22:29 +0100

    Q3

--- TP_RA/TP_RA.cpp
+++ TP_RA/TP_RA.cpp
@@ -1,32 +1,55 @@
 // TP_RA.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
 #include <iostream>
-#include <fstream>
-#include <string>
 
-#include <opencv2/core/core.hpp>
-#include "opencv2/core/utility.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/calib3d.hpp"
+#include "aruco/aruco.h"
+#include "aruco/marker.h"
+#include "aruco/markerdetector.h"
+#include "aruco/markerdetector_impl.h"
 
+using namespace cv;
 using namespace std;
+using namespace aruco;
 
-int main()
+string imagePath;
+Mat img;
+_ARUCO_MarkerDetector_H
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    cout << "Image path : ";
+    cin >> imagePath;
+
+    img = imread(imagePath);
+
+    if (!img.data) {
+        cout << "Open Image Failed" << endl;
+        return 0;
+    }
+    else {
+        cout << "Open Image Successful" << endl;
+    }
     
+    MarkerDetector myDetector;
+
+    for (auto m : myDetector.detect(img)) {
+        cout << m << endl;
+        m.draw(img);
+    }
+
+    
+
+
+
+
+
+
+
+
+    return 1;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
